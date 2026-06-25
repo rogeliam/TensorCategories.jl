5186ad9a55e01eabc65d247eb5e1025bd5830b35
Ulrich Thiel <ulrich.thiel@math.rptu.de>
2026-06-25 10:14:46 +0200

    Updated paper_code_listings to now match paper

--- computations/center_paper/paper_code_listings.jl
+++ computations/center_paper/paper_code_listings.jl
@@ -4,7 +4,7 @@
 println("Loading packages")
 using TensorCategories, Oscar
 
-println("Section 6")
+println("Section 6: Ising category")
 
 function ising_cat(F::Ring, a::RingElem)
     C = six_j_category(F,["1", "𝜒", "X"])
@@ -32,32 +32,47 @@ function ising_cat(F::Ring, a::RingElem)
 end
 
 K,r2 = quadratic_field(2)
-I = ising_cat(K,r2)
-a,b,c = simples(I)
-C = center(I)
-simples(C)
-H = End(C[4])
+C = ising_cat(K,r2)
+a,b,c = simples(C)
+
+Z = center(C)
+S = simples(Z)
+
+H = End(S[4])
 minpoly.(basis(H))
 
-Kx,x = base_ring(I)[:x]
+Kx,x = base_ring(C)[:x]
 L,i = number_field(x^2+1, "i")
 L, = absolute_simple_field(L)
-C2 = C ⊗ L
-simples(C2)
-_,f = minpoly.(basis(End(C2[6])))
+ZL = Z ⊗ L
+simples(ZL)
+
+_,f = minpoly.(basis(End(ZL[6])))
 M,a = number_field(f,"a")
 M, = absolute_simple_field(M)
-simples(C2 ⊗ M) 
+simples(ZL ⊗ M)
 simplify(absolute_simple_field(M)[1])[1]
 
-print_multiplication_table(C)
-smatrix(C)
+print_multiplication_table(S, ["X$i" for i in eachindex(S)])
+smatrix(Z)
 
-half_braiding(C[4])
-R = endomorphism_ring(C[4])
+half_braiding(Z[4])
+R = endomorphism_ring(Z[4])
 basis(R)
 
-println("Section 7")
+println("Section 7: AnyonWiki")
+
+C = anyonwiki(3,1,0,3,1,1,1);
+Z = center(C);
+simples(Z);
+Z2 = split(Z)[1];
+Z3 = six_j_category(Z2);
+
+# Avoid overwrite error
+rm("/tmp/TensorCategories-section7-test"; recursive=true, force=true)
+
+save_fusion_category(Z3,"/tmp","TensorCategories-section7-test");
+Z4 = load_fusion_category("/tmp/TensorCategories-section7-test");
 
 C = anyonwiki(4,1,2,4,1,0,1)
 Z = anyonwiki_center(4,1,2,4,1,0,1)
@@ -67,8 +82,38 @@ simples(Z)
 print_multiplication_table(multiplication_table(Z))
 view(smatrix(Z),1:4,1:4)
 diagonal(tmatrix(Z))
+
 F_symbols(Z)
 R_symbols(Z)
-numeric_F_symbols(Z, precision = 8) 
+num_F_symbs = numeric_F_symbols(Z, precision = 8)
 numeric_R_symbols(Z, precision = 8)
 
+# Avoid overwrite error
+rm("/tmp/TensorCategories-section7-test"; recursive=true, force=true)
+
+numeric_symbols_to_csv("/tmp/TensorCategories-section7-test", num_F_symbs);
+load_numeric_fusion_category("/tmp/TensorCategories-section7-test")
+
+rm("/tmp/TensorCategories-section7-test"; recursive=true, force=true)
+
+C = anyonwiki(QQBarField(),3,1,0,1,2,1,1)
+Z = center(C)
+simples(Z)
+is_unitary.(half_braiding(Z[9]))
+Z2 = six_j_category(Z)
+is_unitary(associator(Z2[[9,9,9]]...))
+
+println("Extra")
+
+Z = anyonwiki_center(3,1,0,3,1,1,1)
+num_F_symbs = numeric_F_symbols(Z)
+rm("/tmp/TensorCategories-section7-test"; recursive=true, force=true)
+numeric_symbols_to_csv("/tmp/TensorCategories-section7-test", num_F_symbs)
+load_numeric_fusion_category("/tmp/TensorCategories-section7-test")
+rm("/tmp/TensorCategories-section7-test"; recursive=true, force=true)
+
+C = anyonwiki(1,1,0,1,1,1,1)
+Z = center(C)
+simples(Z)
+
+println("Finished")
